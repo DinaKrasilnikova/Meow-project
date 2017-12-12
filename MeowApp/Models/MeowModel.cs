56ae7532919e9db28d5f3997312c283c4b1b1455
DinaKrasilnikova <krasilnikova.dina@gmail.com>
2017-12-12 14:36:33 +0300

    I've found!

---
+++ MeowApp/Models/MeowModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeowApp.Models
+{
+    public class MeowModel
+    {
+        public static int MeowCount(string inputString)
+        {
+            string word = "meow";
+            string[] text;
+            string checkString = "";
+            checkString = inputString.Replace('\n', ' ');
+            checkString = checkString.Replace('\r', ' ');
+            checkString = checkString.ToLower();
+            text = checkString.Split(new char[] { ' ' });
+            int result = 0;
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (text[i] == word)
+                {
+                    result = result + 1;
+                }
+            }
+            return result;
+        }
+    }
+}
