58e51a74916f09c56a811652ed8543eae30391e8
DinaKrasilnikova <krasilnikova.dina@gmail.com>
2017-12-12 16:58:14 +0300

    Some hew things...

---
+++ UnitTestProject1/MeowTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeowApp;
+
+namespace MeowTest
+{
+    [TestClass]
+    public class MeowTest
+    {
+        [TestMethod]
+        public void TestMeowCount()
+        {
+            string test = "I meow cat. Meow \n meow cat";
+            int result = MeowApp.Models.MeowModel.MeowCount(test);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5c66189-8a44-4879-9a90-db297c78a231")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
